c933201a6c2dc1496b6edb445728ae85c0996fc3
Luis Thieme <guestuser@MacBook-Pro-von-GuestUser.local>
2023-08-02 10:53:35 +0200

    mario script finished

---
+++ week1/mario-less.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    int counter = 1;
+    do
+    {
+        printf("size: ");
+        scanf("%d", &n);
+    } while (n < 1 || n > 8);
+    
+    
+    int spaces = n;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int k = 0; k < spaces; k++)
+        {
+            printf(" ");
+        }
+        for(int j = 0; j < counter; j++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for(int j = 0; j < counter; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+        counter++;
+        spaces--;
+    }
+    printf("\n");
+
+}
