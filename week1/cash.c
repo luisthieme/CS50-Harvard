9db197dca8cc8d10c3d0d5f4d27caae1b221f812
Luis Thieme <guestuser@MacBook-Pro-von-GuestUser.local>
2023-08-02 16:26:48 +0200

    finished version cash

---
+++ week1/cash.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int cents;
+    int coin_counter = 0;
+
+    printf("Cents: ");
+    scanf("%d", &cents);
+
+    while(cents > 0)
+    {
+        while(cents > 24)
+        {
+            cents -= 25;
+            coin_counter++;
+        }
+        while(cents > 9)
+        {
+            cents -= 10;
+            coin_counter++;
+        }
+        while(cents > 4)
+        {
+            cents -= 5;
+            coin_counter++;
+        }
+        while(cents > 0)
+        {
+            cents -= 1;
+            coin_counter++;
+        }
+        
+    }
+    printf("%d\n", coin_counter);
+}
