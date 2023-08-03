718be910074b4cab059fddca7c218df89741b0c0
Luis Thieme <guestuser@MacBook-Pro-von-GuestUser.local>
2023-08-03 14:32:33 +0200

    readability v1.0

---
+++ week2/readability.c
@@ -0,0 +1,67 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char text[200];
+    char punkt[1] = ".";
+    char ausrufezeichen[1] = "!";
+    char fragezeichen[1] = "?";
+    float letters = 0, sentences = 0, words = 1;
+
+    printf("Enter Text: ");
+    scanf("%[^\n]%*c", text); // in c kann man mit der scanf funktion alleine nur einen string input annehmen bis zum ersten leerzeichen. um das zu umgehen muss man statt %s als placeholder %[^\n]%*c benutzen.
+
+    int lenght = strlen(text);
+
+    for(int i = 0; i < lenght; i++)
+    {
+        char x = text[i];
+        if(x == ' ')
+        {
+            words++;
+        }
+        else if(x == '.')
+        {
+            sentences++;
+        }
+        else if(x == '!')
+        {
+            sentences++;
+        }
+        else if(x == '?')
+        {
+            sentences++;
+        }
+        else if(x == '\'')
+        {}
+        else if(x == '\"')
+        {}
+        else
+        {
+            letters++;
+        }
+    }
+    printf("words: %f letters: %f sentences: %f\n", words, letters, sentences);
+
+    double l = letters / (words / 100);
+    double s = sentences / (words / 100);
+
+    printf("%f %f\n", l, s);
+
+    int index = round(0.0588 * l - 0.296 * s - 15.8);
+    if(index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if(index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %d\n", index);
+    }
+}
+
