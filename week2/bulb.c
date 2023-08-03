ca72a25d3abf82942f213acd6cbf6a51a914210c
Luis Thieme <guestuser@MacBook-Pro-von-GuestUser.local>
2023-08-03 15:36:00 +0200

    bulb

---
+++ week2/bulb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+
+    char text[50];
+    char output[9];
+
+    printf("Enter message: ");
+    scanf("%[^\n]%*c", text);
+
+    int lenght = strlen(text);
+
+    for(int i = 0; i < lenght; i++)
+    {
+        for (int j = 7; j >= 0; --j)
+        {
+            putchar((text[i] & (1 << j)) ? '1' : '0' );
+        }
+        putchar('\n');
+    }
+}
