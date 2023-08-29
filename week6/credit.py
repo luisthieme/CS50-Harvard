b73cb2572cc90b5d7663be6042c0ac54b004e41e
Luis Thieme <guestuser@MacBook-Pro-von-GuestUser.local>
2023-08-29 09:59:04 +0200

    fixes credit

--- week6/credit.py
+++ week6/credit.py
@@ -1,23 +1,22 @@
 def main():
     card_number = input("Enter card number: ")
-    length = len(card_number)
-    x = 0
+    card_number_length = len(card_number)
+    current_digit = 0
     normal_sum = 0
     multiplied_sum = 0
 
-    while x < length:
-        last_digit = int(card_number[length - x - 1])
+    while current_digit < card_number_length:
+        last_digit = int(card_number[card_number_length - current_digit - 1])
         normal_sum += last_digit
-        if length - x - 2 >= 0:
-            sec_to_last_digit = int(card_number[length - x - 2])
+        if card_number_length - current_digit - 2 >= 0:
+            sec_to_last_digit = int(card_number[card_number_length - current_digit - 2])
             sec_to_last_digit *= 2
             multiplied_sum += digit_sum(sec_to_last_digit)
-        x += 2
+        current_digit += 2
     if (normal_sum + multiplied_sum) % 10 == 0:
         get_provider(card_number)
     else:
         print("INVALID")
-
 
 def digit_sum(number):
     sum = 0
@@ -25,7 +24,6 @@ def digit_sum(number):
         sum += number % 10
         number //= 10
     return sum
-
 
 def get_provider(card_number):
     if int(card_number[0]) == 3 and (int(card_number[1]) == 4 or int(card_number[1]) == 7):
