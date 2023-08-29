7f2c9c5b1d1014665a1bfa75ae0c4e1de2fca36d
Luis Thieme <guestuser@MacBook-Pro-von-GuestUser.local>
2023-08-29 09:30:33 +0200

    fixes filter-less

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -1,12 +1,11 @@
 #include "helpers.h"
 #include <stdio.h>
 
-// Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
             RGBTRIPLE temp = image[i][j];
             int avg = (temp.rgbtBlue + temp.rgbtGreen + temp.rgbtRed) / 3;
@@ -16,34 +15,34 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             image[i][j] = temp;
         }
     }
+
     return;
 }
 
-// Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
             RGBTRIPLE temp = image[i][j];
-            int sepiaRed = (int) .393 * temp.rgbtRed + .769 * temp.rgbtGreen + .189 * temp.rgbtBlue;
+            int sepiaRed = (int).393 * temp.rgbtRed + .769 * temp.rgbtGreen + .189 * temp.rgbtBlue;
 
-            if(sepiaRed > 255)
+            if (sepiaRed > 255)
             {
                 sepiaRed = 255;
             }
 
-            int sepiaGreen = (int) .349 * temp.rgbtRed + .686 * temp.rgbtGreen + .168 * temp.rgbtBlue;
+            int sepiaGreen = (int).349 * temp.rgbtRed + .686 * temp.rgbtGreen + .168 * temp.rgbtBlue;
 
-            if(sepiaGreen > 255)
+            if (sepiaGreen > 255)
             {
                 sepiaGreen = 255;
             }
 
-            int sepiaBlue = (int) .272 * temp.rgbtRed + .534 * temp.rgbtGreen + .131 * temp.rgbtBlue;
+            int sepiaBlue = (int).272 * temp.rgbtRed + .534 * temp.rgbtGreen + .131 * temp.rgbtBlue;
 
-            if(sepiaBlue > 255)
+            if (sepiaBlue > 255)
             {
                 sepiaBlue = 255;
             }
@@ -54,15 +53,15 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             image[i][j] = temp;
         }
     }
+
     return;
 }
 
-// Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width / 2; j++)
+        for (int j = 0; j < width / 2; j++)
         {
             RGBTRIPLE temp1 = image[i][j];
             RGBTRIPLE temp2 = image[i][width - j - 1];
@@ -70,18 +69,18 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             image[i][width - j - 1] = temp1;
         }
     }
+
     return;
 }
 
-// Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE topLeftPixel = image[i - 1][j -1];
-            RGBTRIPLE topMidPixel = image[i - 1][j]; 
+            RGBTRIPLE topLeftPixel = image[i - 1][j - 1];
+            RGBTRIPLE topMidPixel = image[i - 1][j];
             RGBTRIPLE topRightPixel = image[i - 1][j + 1];
 
             RGBTRIPLE midLeftPixel = image[i][j - 1];
@@ -101,5 +100,6 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtBlue = bluredBlue;
         }
     }
+
     return;
 }
